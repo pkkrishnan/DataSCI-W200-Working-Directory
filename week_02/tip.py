f71b732dc1d574a6282df29d9b7f2ed9e0a6ffb6
PKKrishnan <pkkrishnan@ischool.berkeley.edu>
2021-04-29 15:09:24 -0700

    my first py

---
+++ week_02/tip.py
@@ -0,0 +1,9 @@
+
+price = input("Enter the price of a meal: ")
+
+tip = float(price) * 0.16
+
+total = float(price) + tip
+
+print(" tip for the meal @16% is:$ ", tip)
+print("price for the meal incl tip is:$ ",total)
